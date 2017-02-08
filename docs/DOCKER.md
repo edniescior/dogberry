bb54ca6103dde08ab9f91d3070ff6610aaa89dbf
Niescior, Edward <Edward_Niescior@cable.comcast.com>
2017-02-08 16:05:09 -0500

    Create EAV style test data.

--- docs/DOCKER.md
+++ docs/DOCKER.md
@@ -7,20 +7,21 @@ These notes are written for running [Docker for Mac](https://www.docker.com/prod
 ```
 $ docker version
 Client:
- Version:      1.12.5
- API version:  1.24
- Go version:   go1.6.4
- Git commit:   7392c3b
- Built:        Fri Dec 16 06:14:34 2016
+ Version:      1.13.0
+ API version:  1.25
+ Go version:   go1.7.3
+ Git commit:   49bf474
+ Built:        Wed Jan 18 16:20:26 2017
  OS/Arch:      darwin/amd64
 
 Server:
- Version:      1.12.5
- API version:  1.24
- Go version:   go1.6.4
- Git commit:   7392c3b
- Built:        Fri Dec 16 06:14:34 2016
+ Version:      1.13.0
+ API version:  1.25 (minimum version 1.12)
+ Go version:   go1.7.3
+ Git commit:   49bf474
+ Built:        Wed Jan 18 16:20:26 2017
  OS/Arch:      linux/amd64
+ Experimental: true
  ```
 
 We use Docker Compose to start and stop the containers. The `docker-compose.yml` file is under `src/test/resources/docker`.
@@ -29,7 +30,7 @@ We use Docker Compose to start and stop the containers. The `docker-compose.yml`
 $ cd src/test/resources/docker
 ```
 
-Before starting the containers edit the environment variable file `.env` and set the file paths to match your environment. For example:
+Before starting the containers edit the environment variable file `.env` and set the variables to match your environment. For example:
 
 ```
 DOG_SRC_DIR=/users/your_user/your_repos/dogberry/src
@@ -38,15 +39,16 @@ DOG_SRC_DIR=/users/your_user/your_repos/dogberry/src
 Use Docker Compose to start the containers.
 
 ```
-$ cd src/test/resources/docker
 $ docker-compose up -d
 ```
+
+## Cassandra
 
  The Cassandra create scripts have been mounted to the `/resources_main` and `/resources_test` volumes on the container. Execute these scripts to create the keyspaces and tables:
 
 ```
-$ docker exec -i -t docker_dogberry-cassandra_1 cqlsh -f '/resources_main/create_tables_dogberry.cql'
-$ docker exec -i -t docker_dogberry-cassandra_1 cqlsh -f '/resources_test/create_tables_test.cql'
+$ docker exec -i -t docker_dogberry-cassandra_1 cqlsh -f '/resources_main/DDL/create_tables_dogberry.cql'
+$ docker exec -i -t docker_dogberry-cassandra_1 cqlsh -f '/resources_test/DDL/create_tables_test.cql'
 ```
 
 To stop the containers:
@@ -77,4 +79,13 @@ $ docker exec -i -t docker_dogberry-cassandra_1 /bin/bash
 
 #### Seeding Cassandra with Test Data
 
-TODO
+The raw test data set is a subset of the IMDB database and is available here: https://www.kaggle.com/deepmatrix/imdb-5000-movie-dataset.
+
+To load the test data:
+
+```
+$ docker exec -i -t docker_dogberry-cassandra_1 cqlsh -f '/resources_test/data/insert_eav_test.cql'
+```
+(Note: The `testdata.MovieDataLoader` object was used to create the above files.)
+
+## Kafka
---
+++ src/test/scala/testdata/MovieDataLoader.scala
@@ -0,0 +1,111 @@
+package testdata
+
+import java.io._
+
+import scala._
+import scala.collection.mutable.ListBuffer
+
+/**
+  * A script to load the IMDB data taken from Kaggle (CSV) and transform it into CQL insert statements.
+  * The target tables are be structured differently. One is in the EAV format; the other looks like
+  * a regular RDBMS table.
+  *
+  * Beware. The paths are hard-coded. There's no error checking. This is just a utility object that won't be used
+  * again unless we need to re-generate the test data files again.
+  *
+  * Created by edniescior on 2/3/17.
+  */
+object MovieDataLoader {
+
+  /**
+    * Escape CQL special chars.
+    *
+    * @param inStr the string with special chars
+    * @return the string with special chars escaped
+    */
+  def escape(inStr: String): String = {
+    //val ESCAPE_CHARS = List("+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":")
+    val ESCAPE_CHARS = List("{", "}")
+    def escapeChar(str: String, chrs: List[String]): String = chrs match {
+      case (Nil) => str
+      case (c :: cs) => escapeChar(str.replace(c, "\\\\" + c), cs)
+    }
+
+    escapeChar(inStr.trim, ESCAPE_CHARS)
+      .replaceAll("'", "''")  // ' is escaped differently from the rest
+      .replaceAll("\\P{Print}", "")  // strip out non printable chars
+  }
+
+  /**
+    * Read in the data source CSV file. A header is expected.
+    *
+    * @param path the path to the CSV file
+    * @return a List of records. Each record is a map where the key is the column heading.
+    */
+  def loadFile(path: String): List[Map[String, String]] = {
+    /* we need to fudge the timestamp values so we generate them randomly */
+    val r = new scala.util.Random(100)
+    val createdT = System.currentTimeMillis() // use this as the created timestamp
+
+    val bufferedSource = io.Source.fromFile(path)
+
+    var header = List[String]()
+    val records = ListBuffer[Map[String, String]]()
+    for ((line, count) <- bufferedSource.getLines.zipWithIndex) {
+      val cols = line.split(",").map(escape(_))
+
+      if (count == 0) {
+        // deal with the header, adding the extra column headers we need
+        header = cols.toList :+ "created" :+ "updated" :+ "key"
+      } else {
+        // add the extra column values and combine with headers
+        val thisCol = cols.toList :+ createdT.toString :+ (createdT + r.nextInt(10000000)).toString :+ count.toString
+        records += (header zip thisCol).toMap
+      }
+    }
+    bufferedSource.close
+    records.toList
+  }
+
+  /**
+    * Given a record, format it into a group of insert statements for an EAV style table.
+    * Null values are filtered out.
+    *
+    * @param record a map representing a record where the key is the column heading.
+    * @return a string of insert statements (separated by a newline char).
+    */
+  def formatToEAV(record: Map[String, String]): String = {
+    // subtract 1 for key, which we don't need inserted
+    val fields = new Array[String](record.size - 1)
+    val key = record("key")
+    var i = 0
+    for ((k, v) <- record if k != "key" && v != "") {
+      fields(i) = (s"INSERT INTO test.eav(ent, attr, value) VALUES ('${key}', '${k}', '${v}');")
+      i = i + 1
+    }
+    fields.slice(0, i).mkString("\n") // slice to strip out the nulls
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    // load the source data
+    val records = loadFile("./src/test/resources/data/movie_metadata.csv")
+    println(s"Loaded ${records.size} records.")
+
+    val pw = new PrintWriter(new File("./src/test/resources/data/insert_eav_test.cql"), "UTF-8")
+
+    // print the EAV data
+    pw.println("\n-- EAV Test Data --\n\nTRUNCATE TABLE test.eav;\n")
+    var count = 0
+    for (record <- records if count <= 50000000) { // limit the num of rows if needed
+      pw.println(formatToEAV(record))
+      count = count + 1
+    }
+    println(s"Wrote ${count} EAV records.")
+
+    pw.close()
+
+  }
+
+
+}
